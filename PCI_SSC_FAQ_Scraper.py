beacddcb3e2b918e7faa23a7fd1af03b2e988011
tbacon <tyler@bacon.io>
2017-07-04 00:16:53 -0700

    Update PCI_SSC_FAQ_Scraper.py

--- PCI_SSC_FAQ_Scraper.py
+++ PCI_SSC_FAQ_Scraper.py
@@ -4,13 +4,6 @@ import lxml
 import json
 import re
 
-# This is a quick Python script that scrapes the PCI SSC's website for FAQ
-# article numbers, creation dates, titles, and corresponding links.
-# Their site doesn't include an index that allows you to view all the FAQ
-# titles in one place (which can be helpful when quickly searching for
-# information on a topic) so I built this.
-# Inspired by github.com/robchahin's previous scraper.
-
 # Define the main scraping/storing function
 def scrape(num):
 
